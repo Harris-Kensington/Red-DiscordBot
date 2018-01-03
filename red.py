ce3278d166cc8abead6410f92fd65e74dc573f2d
Harris-Kensington <34690827+Harris-Kensington@users.noreply.github.com>
2018-01-03 20:40:47 +0000

    Update red.py

--- red.py
+++ red.py
@@ -37,7 +37,7 @@ from io import TextIOWrapper
 #                 https://github.com/Rapptz/RoboDanny/
 #
 
-description = "Red - A multifunction Discord bot by Twentysix"
+description = "Center Parcs Bot - A multifunction Discord bot by WilliamJPercy"
 
 
 class Bot(commands.Bot):
